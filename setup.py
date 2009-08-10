a45152df5bebc4d21cc1f6c87a580c1acdb98c6a
Simeon Visser <simeon87@gmail.com>
2009-08-10 23:52:35 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name="palabra"
+    , version="0.1"
+    , license="GNU General Public License (GPL)"
+    , author="Simeon Visser"
+    , author_email="simeonvisser@gmail.com"
+    , url="http://bitbucket.org/svisser/palabra"
+    , packages=['palabra']
+    )
