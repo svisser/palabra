58550f1908f6661526fb86a4d22c37d486e5dbf2
Simeon Visser <simeon87@gmail.com>
2009-08-11 14:49:37 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 
 setup(name="palabra"
     , version="0.1"
-    , license="GNU General Public License (GPL)"
+    , license="GNU General Public License"
     , author="Simeon Visser"
     , author_email="simeonvisser@gmail.com"
+    , description="A crossword editor"
     , url="http://bitbucket.org/svisser/palabra"
     , packages=['palabra']
     )
