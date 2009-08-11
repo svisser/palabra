b6d77b64375e983194ebcd86c2c70a307ef50dbb
Simeon Visser <simeon87@gmail.com>
2009-08-11 22:48:03 +0200

    Updated setup.py with classifiers

--- setup.py
+++ setup.py
@@ -3,16 +3,26 @@ import sys
 
 from distutils.core import setup
 
-if sys.version < '2.4':
-    print "Python 2.4 is required to run Palabra."
-else:
-    setup(name="palabra"
-        , version="0.1"
-        , license="GNU General Public License"
-        , author="Simeon Visser"
-        , author_email="simeonvisser@gmail.com"
-        , description="A crossword editor"
-        , url="http://bitbucket.org/svisser/palabra"
-        , packages=['palabra']
-        , requires=['gtk', 'pygtk (>=2.8)', 'lxml']
-        )
+if sys.version_info < (2, 4):
+    sys.exit("ERROR: Python 2.4 is required to run Palabra.")
+
+setup(name="palabra"
+    , version="0.1"
+    , license="GNU General Public License (GPL)"
+    , author="Simeon Visser"
+    , author_email="simeonvisser@gmail.com"
+    , description="A crossword editor"
+    , url="http://bitbucket.org/svisser/palabra"
+    , packages=['palabra']
+    , requires=['gtk', 'pygtk (>=2.8)', 'lxml']
+    # see http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    , classifiers=[ 
+        "Development Status :: 4 - Beta"
+        , "Environment :: X11 Applications :: GTK"
+        , "Intended Audience :: End Users/Desktop"
+        , "License :: OSI Approved :: GNU General Public License (GPL)"
+        , "Operating System :: POSIX :: Linux" # for now
+        , "Programming Language :: Python"
+        , "Topic :: Games/Entertainment :: Puzzle Games"
+    ]
+    )
