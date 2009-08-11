06170d0cdbd6178d8be98e44beb76870e28e9df1
Simeon Visser <simeon87@gmail.com>
2009-08-11 22:05:23 +0200

    Enforce dependencies through setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 
+import sys
+
 from distutils.core import setup
 
-setup(name="palabra"
-    , version="0.1"
-    , license="GNU General Public License"
-    , author="Simeon Visser"
-    , author_email="simeonvisser@gmail.com"
-    , description="A crossword editor"
-    , url="http://bitbucket.org/svisser/palabra"
-    , packages=['palabra']
-    )
+if sys.version < '2.4':
+    print "Python 2.4 is required to run Palabra."
+else:
+    setup(name="palabra"
+        , version="0.1"
+        , license="GNU General Public License"
+        , author="Simeon Visser"
+        , author_email="simeonvisser@gmail.com"
+        , description="A crossword editor"
+        , url="http://bitbucket.org/svisser/palabra"
+        , packages=['palabra']
+        , requires=['gtk', 'pygtk (>=2.8)', 'lxml']
+        )
