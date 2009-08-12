278aec74406fcdadb721f199a503f75578733d11
Simeon Visser <simeon87@gmail.com>
2009-08-12 16:40:37 +0200

    Reordered widgets in appearance dialog

--- palabralib/appearance.py
+++ palabralib/appearance.py
@@ -68,7 +68,7 @@ class AppearanceDialog(gtk.Dialog):
             , palabra_window, gtk.DIALOG_MODAL)
         self.palabra_window = palabra_window
         self.properties = properties
-        self.set_size_request(480, 360)
+        self.set_size_request(480, 240)
         
         main = gtk.VBox(False, 0)
         main.set_spacing(18)
@@ -87,7 +87,7 @@ class AppearanceDialog(gtk.Dialog):
         main = gtk.VBox(False, 0)
         main.set_spacing(6)
         
-        table = gtk.Table(9, 3)
+        table = gtk.Table(9, 6)
         table.set_col_spacings(6)
         table.set_row_spacings(3)
         main.pack_start(table, False, False, 0)
@@ -113,6 +113,11 @@ class AppearanceDialog(gtk.Dialog):
             table.attach(c1, 1, 2, y, y + 1, 0, 0)
             if c2 is not None:
                 table.attach(c2, 2, 3, y, y + 1, 0, 0)
+        def create_row_two(table, y, label, c1, c2=None):
+            table.attach(label, 3, 4, y, y + 1, gtk.FILL, gtk.FILL)
+            table.attach(c1, 4, 5, y, y + 1, 0, 0)
+            if c2 is not None:
+                table.attach(c2, 5, 6, y, y + 1, 0, 0)
         
         label = create_label(u"Color")
         table.attach(label, 1, 2, 0, 1, gtk.FILL, gtk.FILL)
@@ -133,38 +138,38 @@ class AppearanceDialog(gtk.Dialog):
         
         # cells
         label = create_label(u"Color")
-        table.attach(label, 1, 2, 3, 4, gtk.FILL, gtk.FILL)
+        table.attach(label, 4, 5, 0, 1, gtk.FILL, gtk.FILL)
         label = create_label(u"Size")
-        table.attach(label, 2, 3, 3, 4, gtk.FILL, gtk.FILL)
+        table.attach(label, 5, 6, 0, 1, gtk.FILL, gtk.FILL)
         
         label = create_label(u"Cell:")
         self.cell_color_button = create_color_button(self.properties.cell["color"])
         adj = gtk.Adjustment(self.properties.cell["size"], 32, 128, 1, 0, 0)
         self.cell_size_spinner = gtk.SpinButton(adj, 0.0, 0)
-        create_row(table, 4, label, self.cell_color_button, self.cell_size_spinner)
+        create_row_two(table, 1, label, self.cell_color_button, self.cell_size_spinner)
         
         # blocks
         label = create_label(u"Color")
-        table.attach(label, 1, 2, 5, 6, gtk.FILL, gtk.FILL)
+        table.attach(label, 4, 5, 2, 3, gtk.FILL, gtk.FILL)
         label = create_label(u"Margin (%)")
-        table.attach(label, 2, 3, 5, 6, gtk.FILL, gtk.FILL)
+        table.attach(label, 5, 6, 2, 3, gtk.FILL, gtk.FILL)
         
         current = self.properties.block["margin"]
         label = create_label(u"Blocks:")
         self.block_color_button = create_color_button(self.properties.block["color"])
         adj = gtk.Adjustment(current, 0, 49, 1, 0, 0)
         self.block_margin_spinner = gtk.SpinButton(adj, 0.0, 0)
-        create_row(table, 6, label, self.block_color_button, self.block_margin_spinner)
+        create_row_two(table, 3, label, self.block_color_button, self.block_margin_spinner)
         
         # letters
         label = create_label(u"Letter:")
         self.char_color_button = create_color_button(self.properties.char["color"])
-        create_row(table, 7, label, self.char_color_button)
+        create_row(table, 3, label, self.char_color_button)
         
         # numbers
         label = create_label(u"Number:")
         self.number_color_button = create_color_button(self.properties.number["color"])
-        create_row(table, 8, label, self.number_color_button)
+        create_row(table, 4, label, self.number_color_button)
         
         return main
         
