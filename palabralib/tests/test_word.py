0d43c15b13d87a1e3cc7de254bf6ccd1134f9c09
Simeon Visser <simeon87@gmail.com>
2009-09-27 00:07:46 +0200

    Fixed unit tests

--- palabralib/tests/test_word.py
+++ palabralib/tests/test_word.py
@@ -61,13 +61,13 @@ class WordTestCase(unittest.TestCase):
     
     def testSearchBasic(self):
         self.wordlist.add_word(self.word)
-        self.assertEquals(self.wordlist.search(self.length, []), [self.word])
-        self.assertEquals(self.wordlist.search(self.length, self.constraints), [self.word])
+        self.assertEquals(self.wordlist.search(self.length, [])[0][0], self.word)
+        self.assertEquals(self.wordlist.search(self.length, self.constraints)[0][0], self.word)
         
         cs = [(0, "p"), (1, "a")]
         self.wordlist.add_word(self.word2)
-        self.assertEquals(self.wordlist.search(self.length, cs), [self.word])
-        self.assertEquals(self.wordlist.search(self.length2, cs), [self.word2])
+        self.assertEquals(self.wordlist.search(self.length, cs)[0][0], self.word)
+        self.assertEquals(self.wordlist.search(self.length2, cs)[0][0], self.word2)
         
     def testSearchMore(self):
         self.wordlist.add_word(self.word)
@@ -79,17 +79,18 @@ class WordTestCase(unittest.TestCase):
         self.wordlist.add_word("oasis")
         self.wordlist.add_word("trunk")
         
-        self.assertEquals(self.wordlist.search(5, [(0, "p")]), ["peach"])
-        self.assertEquals(self.wordlist.search(5, [(0, "a")]), ["azure"])
+        self.assertEquals(self.wordlist.search(5, [(0, "p")])[0][0], "peach")
+        self.assertEquals(self.wordlist.search(5, [(0, "a")])[0][0], "azure")
         rs = self.wordlist.search(5, [(0, "r")])
-        self.assertEquals("reach" in rs, True)
-        self.assertEquals("roast" in rs, True)
-        self.assertEquals(self.wordlist.search(5, [(0, "o")]), ["oasis"])
-        self.assertEquals(self.wordlist.search(5, [(0, "t")]), ["trunk"])
+        rss = [w for w, x in rs]
+        self.assertEquals("reach" in rss, True)
+        self.assertEquals("roast" in rss, True)
+        self.assertEquals(self.wordlist.search(5, [(0, "o")])[0][0], "oasis")
+        self.assertEquals(self.wordlist.search(5, [(0, "t")])[0][0], "trunk")
         
         # all first characters
         css = [(0, 5, [(0, c)]) for c in "parrot"]
-        self.assertEquals(self.wordlist.search(self.length2, [], css), [self.word2])
+        self.assertEquals(self.wordlist.search(self.length2, [], css)[0][0], self.word2)
         
         # diagonal
         self.wordlist.add_word("cabin")
@@ -99,4 +100,4 @@ class WordTestCase(unittest.TestCase):
         self.wordlist.add_word("adrift")
         
         css = [(i, 5, [(i, self.word2[i])]) for i in xrange(5)] + [(5, 6, [(5, "t")])]
-        self.assertEquals(self.wordlist.search(self.length2, [], css), [self.word2])
+        self.assertEquals(self.wordlist.search(self.length2, [], css)[0][0], self.word2)
