e565da9b887ea547bb24cb16e7c08add99da3801
Simeon Visser <simeon87@gmail.com>
2009-10-10 21:00:52 +0200

    Pattern files can now be viewed in pattern file manager

--- palabralib/pattern.py
+++ palabralib/pattern.py
@@ -15,8 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import gobject
 import gtk
 
+from files import read_containers
 from grid import Grid
 from newpuzzle import GridPreview
 
@@ -25,22 +27,61 @@ class PatternFileEditor(gtk.Dialog):
         gtk.Dialog.__init__(self, u"Pattern file manager"
             , palabra_window, gtk.DIALOG_MODAL)
         self.palabra_window = palabra_window
-        self.set_size_request(640, 480)
+        self.set_size_request(640, 640)
         
         self.preview = GridPreview()
-        self.preview.set_size_request(200, -1)
-        self.preview.display(Grid(9, 9))
+        self.preview.set_size_request(200, 250)
+        
+        self.patterns = read_containers(self.palabra_window.pattern_files)
+        
+        self.store = gtk.TreeStore(str, gobject.TYPE_PYOBJECT)
+        for (f, metadata, data) in self.patterns:
+            parent = self.store.append(None, [f, None])
+            for grid in data:
+                blocks = grid.count_blocks()
+                words = grid.count_words()
+                s = "".join([str(words), " words, ", str(blocks), " blocks"])
+                self.store.append(parent, [s, grid])
+        
+        self.tree = gtk.TreeView(self.store)
+        self.tree.get_selection().connect("changed", self.on_selection_changed)
+        cell = gtk.CellRendererText()
+        column = gtk.TreeViewColumn(u"Pattern files")
+        column.pack_start(cell, True)
+        column.set_attributes(cell, text=0)
+        self.tree.append_column(column)
+        
+        buttonbox = gtk.HButtonBox()
+        buttonbox.set_layout(gtk.BUTTONBOX_END)
+        
+        add_button = gtk.Button(stock=gtk.STOCK_ADD)
+        buttonbox.pack_start(add_button, False, False, 0)
+        
+        remove_button = gtk.Button(stock=gtk.STOCK_REMOVE)
+        buttonbox.pack_start(remove_button, False, False, 0)
+        
+        options_vbox = gtk.VBox(False, 6)
+        options_vbox.pack_start(self.tree, True, True, 0)
+        options_vbox.pack_start(buttonbox, False, False, 0)
+        options_vbox.pack_start(self.preview, False, False, 0)
         
         hbox = gtk.HBox(False, 0)
         hbox.set_border_width(12)
         hbox.set_spacing(18)
         
-        hbox.pack_start(self.preview, True, True, 0)
+        hbox.pack_start(options_vbox, True, True, 0)
         
         self.vbox.pack_start(hbox, True, True, 0)
         
         self.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
         self.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
+        
+    def on_selection_changed(self, selection):
+        store, it = selection.get_selected()
+        
+        parent = store.iter_parent(it)
+        display = store.get_value(it, 1) if parent is not None else Grid(0, 0)
+        self.preview.display(display)
 
 class Pattern:
     def __init__(self):
