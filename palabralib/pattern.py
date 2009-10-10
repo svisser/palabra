882f9f355c077cc433055eda0a70f24e63464317
Simeon Visser <simeon87@gmail.com>
2009-10-10 23:27:47 +0200

    Pattern files can now be added and removed

--- palabralib/pattern.py
+++ palabralib/pattern.py
@@ -18,7 +18,7 @@
 import gobject
 import gtk
 
-from files import read_containers
+from files import read_container, read_containers
 from grid import Grid
 from newpuzzle import GridPreview
 
@@ -35,15 +35,10 @@ class PatternFileEditor(gtk.Dialog):
         self.patterns = read_containers(self.palabra_window.pattern_files)
         
         self.store = gtk.TreeStore(str, gobject.TYPE_PYOBJECT)
-        for (f, metadata, data) in self.patterns:
-            parent = self.store.append(None, [f, None])
-            for grid in data:
-                blocks = grid.count_blocks()
-                words = grid.count_words()
-                s = "".join([str(words), " words, ", str(blocks), " blocks"])
-                self.store.append(parent, [s, grid])
+        self.display_files()
         
         self.tree = gtk.TreeView(self.store)
+        self.tree.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
         self.tree.get_selection().connect("changed", self.on_selection_changed)
         cell = gtk.CellRendererText()
         column = gtk.TreeViewColumn(u"Pattern files")
@@ -55,10 +50,13 @@ class PatternFileEditor(gtk.Dialog):
         buttonbox.set_layout(gtk.BUTTONBOX_END)
         
         add_button = gtk.Button(stock=gtk.STOCK_ADD)
+        add_button.connect("clicked", lambda button: self.add_file())
         buttonbox.pack_start(add_button, False, False, 0)
         
-        remove_button = gtk.Button(stock=gtk.STOCK_REMOVE)
-        buttonbox.pack_start(remove_button, False, False, 0)
+        self.remove_button = gtk.Button(stock=gtk.STOCK_REMOVE)
+        self.remove_button.connect("clicked", lambda button: self.remove_file())
+        self.remove_button.set_sensitive(False)
+        buttonbox.pack_start(self.remove_button, False, False, 0)
         
         options_vbox = gtk.VBox(False, 6)
         options_vbox.pack_start(self.tree, True, True, 0)
@@ -76,12 +74,51 @@ class PatternFileEditor(gtk.Dialog):
         self.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
         self.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
         
-    def on_selection_changed(self, selection):
-        store, it = selection.get_selected()
+    def display_files(self):
+        self.store.clear()
+        for (f, metadata, data) in self.patterns:
+            parent = self.store.append(None, [f, None])
+            for grid in data:
+                blocks = grid.count_blocks()
+                words = grid.count_words()
+                s = "".join([str(words), " words, ", str(blocks), " blocks"])
+                self.store.append(parent, [s, grid])
         
-        parent = store.iter_parent(it)
-        display = store.get_value(it, 1) if parent is not None else Grid(0, 0)
-        self.preview.display(display)
+    def add_file(self):
+        dialog = gtk.FileChooserDialog(u"Add pattern file"
+            , self
+            , gtk.FILE_CHOOSER_ACTION_OPEN
+            , (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL
+            , gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        dialog.show_all()
+        response = dialog.run()
+        if response == gtk.RESPONSE_OK:
+            path = dialog.get_filename()
+            self.palabra_window.pattern_files.append(path)
+            
+            metadata, data = read_container(path)
+            self.patterns.append((path, metadata, data))
+            self.display_files()
+        dialog.destroy()
+        
+    def remove_file(self):
+        store, paths = self.tree.get_selection().get_selected_rows()
+        if len(paths) == 1:
+            it = store.get_iter(paths[0])
+            filename = store.get_value(it, 0)
+            store.remove(it)
+            self.palabra_window.pattern_files.remove(filename)
+        
+    def on_selection_changed(self, selection):
+        store, paths = selection.get_selected_rows()
+        self.remove_button.set_sensitive(False)
+        for p in paths:
+            it = store.get_iter(p)
+            parent = store.iter_parent(it)
+            display = store.get_value(it, 1) if parent is not None else Grid(0, 0)
+            self.preview.display(display)
+            if len(paths) == 1 and parent is None:
+                self.remove_button.set_sensitive(True)
 
 class Pattern:
     def __init__(self):
