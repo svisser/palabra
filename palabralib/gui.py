ec300bdb06e75747423c2cdeddd3164b59b873ad
Simeon Visser <simeon87@gmail.com>
2010-06-05 16:01:45 +1000

    Added email to credits screen

--- palabralib/gui.py
+++ palabralib/gui.py
@@ -1177,7 +1177,7 @@ class PalabraWindow(gtk.Window):
         dialog.set_program_name(u"Palabra")
         dialog.set_comments(u"Crossword creation software")
         dialog.set_version(constants.VERSION)
-        dialog.set_authors([u"Simeon Visser"])
+        dialog.set_authors([u"Simeon Visser <simeonvisser@gmail.com>"])
         dialog.set_copyright(u"Copyright © 2009 - 2010 Simeon Visser")
         def on_click_website(dialog, link):
             webbrowser.open(link)
