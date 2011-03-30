4a982a893d26471dd895b562caa7413ddfe17d83
Simeon Visser <simeon87@gmail.com>
2011-03-30 16:06:16 +0200

    Updated copyright notice in source files

--- palabralib/gui.py
+++ palabralib/gui.py
@@ -1,7 +1,7 @@
 # This file is part of Palabra
 # coding: utf-8
 #
-# Copyright (C) 2009 - 2010 Simeon Visser
+# Copyright (C) 2009 - 2011 Simeon Visser
 #
 # Palabra is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- palabralib/tests/test_transform.py
+++ palabralib/tests/test_transform.py
@@ -1,6 +1,6 @@
 # This file is part of Palabra
 #
-# Copyright (C) 2009 - 2010 Simeon Visser
+# Copyright (C) 2009 - 2011 Simeon Visser
 #
 # Palabra is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
