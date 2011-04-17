f74105ae85560546093bcc39bdad015e8afb74cf
Simeon Visser <simeon87@gmail.com>
2011-04-17 19:36:01 +0200

    Further work to improve setup

--- setup.py
+++ setup.py
@@ -14,6 +14,19 @@ def create_ext(e):
 EXTS = [create_ext(e) for e in ['grid', 'view', 'word']]
 ext_modules = [Extension(n, sources=s) for n, s in EXTS]
 
+# see http://pypi.python.org/pypi?%3Aaction=list_classifiers
+classifiers = [ 
+"Development Status :: 4 - Beta"
+, "Environment :: X11 Applications :: GTK"
+, "Intended Audience :: End Users/Desktop"
+, "License :: OSI Approved :: GNU General Public License (GPL)"
+, "Operating System :: POSIX :: Linux" # for now
+, "Natural Language :: English"
+, "Programming Language :: C"
+, "Programming Language :: Python :: 2.6"
+, "Topic :: Games/Entertainment :: Puzzle Games"
+]
+
 setup(name="palabra"
     , version=constants.VERSION
     , license="GNU General Public License (GPL)"
@@ -21,16 +34,10 @@ setup(name="palabra"
     , author_email="simeonvisser@gmail.com"
     , description="A crossword editor"
     , url=constants.WEBSITE
+    , requires=['lxml']
     , packages=['palabralib']
-    , scripts=['palabra']
-    # see http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    , classifiers=[ 
-        "Development Status :: 4 - Beta"
-        , "Environment :: X11 Applications :: GTK"
-        , "Intended Audience :: End Users/Desktop"
-        , "License :: OSI Approved :: GNU General Public License (GPL)"
-        , "Operating System :: POSIX :: Linux" # for now
-        , "Programming Language :: Python"
-        , "Topic :: Games/Entertainment :: Puzzle Games"
-        ]
+    , package_data={'': ['xml/patterns.xml']}
+    , entry_points={'console_scripts':['palabra = palabra']}
+    , classifiers=classifiers
+    , include_dirs=['palabralib']
     , ext_modules=ext_modules)
